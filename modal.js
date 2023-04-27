5b83f35d083daf2a43ff2cc410ee8ea9949141cb
MisterFried <wilbaspeyras@gmail.com>
2023-04-27 11:26:21 +0300

    close modal

--- modal.js
+++ modal.js
@@ -8,13 +8,22 @@ function editNav() {
 const modalContainer = document.querySelector(".modal-container");
 const openModalBtn = document.querySelectorAll(".open-modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector("#close-modal");
 
-// launch modal event
+// open modal event listener
 openModalBtn.forEach((btn) =>
-	btn.addEventListener("click", launchModal)
+	btn.addEventListener("click", openModal)
 );
 
-// launch modal form
-function launchModal() {
-	modalContainer.style.display = "block";
+// open modal function
+function openModal() {
+	modalContainer.style.display = "flex";
+}
+
+// close modal event listener
+closeButton.addEventListener("click",closeModal);
+
+//close modal function
+function closeModal() {
+  modalContainer.style.display = "none";
 }
