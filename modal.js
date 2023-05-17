0b365401f8ae8c10e071da1224ed1bcd630c6201
wilfried <wilbaspeyras@gmail.com>
2023-05-17 09:49:45 +0200

    feat : add navbar closing when clocking outside

--- modal.js
+++ modal.js
@@ -1,8 +1,26 @@
 //***** Header navigation bar *****//
+//Open / Close navbar
 function openNav() {
 	document.querySelector("#header-navbar").classList.toggle("open");
 	document.querySelector("#header-navbar").classList.toggle("close");
 }
+
+//Close navbar
+function closeNav () {
+	if (document.querySelector("#header-navbar.open")) {
+		document.querySelector("#header-navbar").classList.remove("open");
+		document.querySelector("#header-navbar").classList.add("close");
+	}
+}
+
+//Close navbar (click outside)
+const body = document.querySelector("body")
+body.addEventListener("click", closeNav);
+
+const navIcon = document.querySelector(".header-mobile-icon");
+const navBar = document.querySelector(".header-navbar");
+navIcon.addEventListener("click", (e) => {e.stopPropagation()});
+navBar.addEventListener("click", (e) => {e.stopPropagation();});
 
 //*************** Modal display ***************//
 const openModalButton = document.querySelector("#open-modal-button");
